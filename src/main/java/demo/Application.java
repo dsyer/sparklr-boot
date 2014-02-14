fab3fb88d70be399520831afb87aea66e0a62dce
Dave Syer <dsyer@gopivotal.com>
2014-02-14 15:43:51 +0000

    WIP

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,106 +1,55 @@
 package demo;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.client.ClientCredentialsTokenEndpointFilter;
-import org.springframework.security.oauth2.provider.client.ClientDetailsUserDetailsService;
-import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
-import org.springframework.security.oauth2.provider.error.OAuth2AuthenticationEntryPoint;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.InMemoryTokenStore;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.oauth2.config.annotation.authentication.configurers.InMemoryClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.OAuth2AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 
 @ComponentScan
 @EnableAutoConfiguration
-@ImportResource("classpath:/spring-servlet.xml")
 @Order(Ordered.LOWEST_PRECEDENCE - 100)
-public class Application extends WebSecurityConfigurerAdapter {
-
-	@Autowired
-	private ClientDetailsService clientDetailsService;
+public class Application extends OAuth2AuthorizationServerConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		// This filter has it's own client-specific authentication manager
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// @formatter:off
-		http.userDetailsService(new ClientDetailsUserDetailsService(
-				clientDetailsService))
-			.exceptionHandling().authenticationEntryPoint(
-				clientAuthenticationEntryPoint())
-		.and()
-			.requestMatchers().antMatchers("/oauth/token")
-		.and()
-		    .authorizeRequests().anyRequest().authenticated()//
-				.and().httpBasic() //
-				.and().anonymous().disable()
-			.exceptionHandling().accessDeniedHandler(
-				new OAuth2AccessDeniedHandler())
-		.and()
-		    .sessionManagement().sessionCreationPolicy(
-				SessionCreationPolicy.STATELESS)
-		.and()
-			.csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/**")).disable()
-		    .addFilterAfter(clientCredentialsTokenEndpointFilter(),
-				BasicAuthenticationFilter.class);
+		 	auth.apply(new InMemoryClientDetailsServiceConfigurer())
+		        .withClient("my-trusted-client")
+		            .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+		            .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+		            .scopes("read", "write", "trust")
+		            .accessTokenValiditySeconds(60)
+ 		    .and()
+		        .withClient("my-client-with-secret")
+		            .authorizedGrantTypes("client_credentials")
+		            .authorities("ROLE_CLIENT")
+		            .scopes("read")
+		            .secret("secret");
 		// @formatter:on
-
 	}
-
-	@Bean
-	public OAuth2AuthenticationEntryPoint clientAuthenticationEntryPoint() {
-		OAuth2AuthenticationEntryPoint entryPoint = new OAuth2AuthenticationEntryPoint();
-		entryPoint.setRealmName("sparklr2/client");
-		entryPoint.setTypeName("Basic");
-		return entryPoint;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+        http
+            .authorizeRequests()
+                .antMatchers("/oauth/token").fullyAuthenticated()
+                .and()
+            .requestMatchers()
+                .antMatchers("/oauth/token")
+                .and()
+            .apply(new OAuth2AuthorizationServerConfigurer());
+    	// @formatter:on
 	}
-
-	@Bean
-	protected ClientCredentialsTokenEndpointFilter clientCredentialsTokenEndpointFilter() {
-		ClientCredentialsTokenEndpointFilter clientCredentialsTokenEndpointFilter = new ClientCredentialsTokenEndpointFilter();
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setUserDetailsService(new ClientDetailsUserDetailsService(
-				clientDetailsService));
-		clientCredentialsTokenEndpointFilter
-				.setAuthenticationManager(new ProviderManager(Arrays
-						.<AuthenticationProvider> asList(provider)));
-		clientCredentialsTokenEndpointFilter.setAuthenticationEntryPoint(clientAuthenticationEntryPoint());
-		return clientCredentialsTokenEndpointFilter;
-	}
-
-	@Bean
-	protected TokenStore tokenStore() {
-		return new InMemoryTokenStore();
-	}
-
-	@Bean
-	public DefaultTokenServices tokenServices() {
-		DefaultTokenServices tokenServices = new DefaultTokenServices();
-		tokenServices.setClientDetailsService(clientDetailsService);
-		tokenServices.setSupportRefreshToken(true);
-		tokenServices.setTokenStore(tokenStore());
-		return tokenServices;
-	}
-
+	
 }
