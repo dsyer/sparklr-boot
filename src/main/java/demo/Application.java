6552da7039a8f8161013529edcb830529f166e16
Dave Syer <dsyer@gopivotal.com>
2014-02-20 15:58:50 +0000

    Update with @Enable*

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.oauth2.config.annotation.authentication.configurers.InMemoryClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.OAuth2AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @ComponentScan
 @EnableAutoConfiguration
@@ -19,13 +18,13 @@ public class Application {
 	}
 
 	@Configuration
-	@Order(Ordered.LOWEST_PRECEDENCE - 100)
-	protected static class OAuth2Config extends OAuth2AuthorizationServerConfigurerAdapter {
+	@EnableAuthorizationServer
+	protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
 		@Override
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		public void configure(AuthenticationManagerBuilder auth) throws Exception {
 			// @formatter:off
-		 	auth.apply(new InMemoryClientDetailsServiceConfigurer())
+		 	auth.apply(new ClientDetailsServiceConfigurer()).inMemory()
 		        .withClient("my-trusted-client")
 		            .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
 		            .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
